9e65356e048b8067865c914dc3a0374553cb356a
MarcosHenriqueFr <marcosfhtrabalho@gmail.com>
2025-04-15 10:05:01 -0300

    Config module-info

--- app-calculo/src/module-info.java
+++ app-calculo/src/module-info.java
@@ -1,3 +1,4 @@
 module app.calculo {
-
+    // Exporta somente as classes que estão visiveis, não as subpastas
+    exports br.com.empresa.app.calculo;
 }
--- app-financeiro/src/module-info.java
+++ app-financeiro/src/module-info.java
@@ -1,3 +1,7 @@
 module app.financeiro {
 
+    // Pacote requerido como padrão
+    // requires java.base;
+
+    requires app.calculo;
 }
